2a10be701d9781aafc2cb15a9308f235b2c1c15a
NazmulAlamShawon <shawonkst15@gmail.com>
2024-01-03 10:22:40 +0600

    add text

--- src/components/Hero/Hero_section.jsx
+++ src/components/Hero/Hero_section.jsx
@@ -6,6 +6,7 @@ const Hero_section = () => {
     <div className="bg-primary flex justify-center items-center text-white border-orange-100 border-spacing-1 ">
       <div className="flex gap-4 m-5 p-4 flex-col ">
         <img src={myImage} alt="Description" />
+        <p className="text-center font-light ">Quick Order and Quick Service</p>
         <button className="outline outline-red-700 animate-pulse  m-3 p-3">
           Order Now
         </button>
