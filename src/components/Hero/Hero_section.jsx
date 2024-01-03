4586a8f5495fceae7c34f81ab1530e682086d448
NazmulAlamShawon <shawonkst15@gmail.com>
2024-01-03 18:32:17 +0600

    add background image

--- src/components/Hero/Hero_section.jsx
+++ src/components/Hero/Hero_section.jsx
@@ -1,12 +1,21 @@
+import React from "react";
 import myImage from "../../../public/pngtree-yellow-gourmet-shading-cartoon-chef-restaurant-recruitment-background-material-image_133951.jpg";
 import { Link } from "react-router-dom";
 
-const Hero_section = () => {
+const HeroSection = () => {
   return (
-    <div className="bg-primary flex justify-center items-center text-white border-orange-100 border-spacing-1 ">
-      <div className="flex gap-4 m-5 p-4 flex-col ">
-        <img src={myImage} alt="Description" />
-        <p className="text-center font-light ">Quick Order and Quick Service</p>
+    <div
+      className="bg-primary flex justify-center items-center text-white border-orange-100 border-spacing-1"
+      style={{
+        backgroundImage: `url(${myImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "650px",
+      }}
+    >
+      <div className="flex gap-4 m-5 p-4 flex-col">
+        <p className="text-center font-light">Quick Order and Quick Service</p>
         <Link to="/order">
           <button className="outline w-full outline-red-700 animate-pulse m-3 p-3">
             Order Now
@@ -16,4 +25,5 @@ const Hero_section = () => {
     </div>
   );
 };
-export default Hero_section;
+
+export default HeroSection;
