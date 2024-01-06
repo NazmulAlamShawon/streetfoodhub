704a42168db8dd77a1573f7259b7224e86a73aa5
NazmulAlamShawon <shawonkst15@gmail.com>
2024-01-06 10:43:52 +0600

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Menu from "./pages/Menu";
-import Order from "./pages/Order";
-import Login from "./pages/Login";
-import OnlineOrder from "./pages/OnlineOrder";
-
-
-
 const App = () => {
-  return (
-     
-    <Routes>
-     
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/order" element={<Order />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/online-order" element={<OnlineOrder></OnlineOrder>} />
-    </Routes>
-  );
+  return <div>App test</div>;
 };
 export default App;
